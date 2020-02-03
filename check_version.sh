53f2eda0b58bf22287df79137a8446d41b73f549
Jon Jagger <jon@jaggersoft.com>
2020-02-03 09:17:11 +0000

    Refactoring; in check_version.sh get image-name from json file

--- check_version.sh
+++ check_version.sh
@@ -1,5 +1,10 @@
 #!/bin/bash -Eeu
 
+readonly REGEX="image_name\": \"(.*)\""
+readonly JSON=`cat docker/image_name.json`
+[[ ${JSON} =~ ${REGEX} ]]
+readonly IMAGE_NAME="${BASH_REMATCH[1]}"
+
 readonly MY_DIR="$( cd "$( dirname "${0}" )" && pwd )"
 readonly EXPECTED=3.8.1
 readonly ACTUAL=$(docker run --rm -it cyberdojofoundation/python sh -c 'python --version')
