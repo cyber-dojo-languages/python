38fc11ad75b52e47436e8df09317e94614e37fd6
Tahir Adil <tahir@kosli.com>
2025-07-17 13:36:04 +0200

    Update python to 3.13.5

--- check_version.sh
+++ check_version.sh
@@ -6,7 +6,7 @@ readonly JSON=`cat docker/image_name.json`
 readonly IMAGE_NAME="${BASH_REMATCH[1]}"
 
 readonly MY_DIR="$( cd "$( dirname "${0}" )" && pwd )"
-readonly EXPECTED=3.13.3
+readonly EXPECTED=3.13.5
 readonly ACTUAL=$(docker run --rm -i ${IMAGE_NAME} sh -c 'python --version')
 
 if echo "${ACTUAL}" | grep -q "${EXPECTED}"; then
